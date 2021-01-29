e07cb01440d6f930c3a674ced6c12588f0ee87d5
tienanhpham252 <78161342+tienanhpham252@users.noreply.github.com>
2021-01-29 07:03:55 +0700

    Add files via upload

---
+++ uartecho_MSP_EXP432P401R_nortos_ccs/Debug/uartecho.d
@@ -0,0 +1,35 @@
+# FIXED
+
+uartecho.obj: ../uartecho.c
+uartecho.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/stdint.h
+uartecho.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/_ti_config.h
+uartecho.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/linkage.h
+uartecho.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/_stdint40.h
+uartecho.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/sys/stdint.h
+uartecho.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/sys/cdefs.h
+uartecho.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/sys/_types.h
+uartecho.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/machine/_types.h
+uartecho.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/machine/_stdint.h
+uartecho.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/sys/_stdint.h
+uartecho.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/stddef.h
+uartecho.obj: C:/ti/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/GPIO.h
+uartecho.obj: C:/ti/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/UART.h
+uartecho.obj: C:/Users/ASUS/workspace_v10/uartecho_MSP_EXP432P401R_nortos_ccs/Debug/syscfg/ti_drivers_config.h
+uartecho.obj: C:/ti/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/DeviceFamily.h
+
+../uartecho.c: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/stdint.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/_ti_config.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/linkage.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/_stdint40.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/sys/stdint.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/sys/cdefs.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/sys/_types.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/machine/_types.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/machine/_stdint.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/sys/_stdint.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.3.LTS/include/stddef.h: 
+C:/ti/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/GPIO.h: 
+C:/ti/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/UART.h: 
+C:/Users/ASUS/workspace_v10/uartecho_MSP_EXP432P401R_nortos_ccs/Debug/syscfg/ti_drivers_config.h: 
+C:/ti/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/DeviceFamily.h: 
